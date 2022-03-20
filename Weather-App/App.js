d395d6348468b4849dfca3bdf1a391ad15912ab4
Robobot399 <phuc_l_nguyen@hotmail.com>
2022-03-20 14:49:32 +0000

    No changes

--- Weather-App/App.js
+++ Weather-App/App.js
@@ -1,21 +1,14 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-
 import * as Font from 'expo-font'; // Import customised fonts
 import { AppLoading } from 'expo'; // Used with importing fonts
 
 /* IMPORTS */
 import { MyDrawer } from './navigation/DrawerNavigator';
 
-
 /*Navigation Settings*/
 import { NavigationContainer, StackActions } from '@react-navigation/native';
-
-
-
-
-
 
 
 const App = () => {
