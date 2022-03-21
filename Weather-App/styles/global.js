55e678deb7b8fd7c499f95779f32fef5c5e0b47a
Robobot399 <phuc_l_nguyen@hotmail.com>
2022-03-21 17:44:39 +0000

    Added wardrobe

--- Weather-App/styles/global.js
+++ Weather-App/styles/global.js
@@ -1,16 +1,40 @@
-import { StyleSheet } from "react-native-web";
+import { StyleSheet } from 'react-native';
+import { borderColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+
 
 export const globalStyles = StyleSheet.create({
     container: {
-        flex: 1,
-        padding: 20,
-        backgroundColor: 'red',
-        //alignItems: 'center',
-        //justifyContent: 'center',
+        flex: 1, // dissapears content if not setting height
+        margin: 24,
+        marginTop: 70,
+    },
+    header: {
+        
     },
     titleText: {
-        //fontFamily:
-        fontSize: 18,
-        color: '#333', 
+        //fontFamily: 'RobotoMono-Regular',
+        fontSize: 24,
+        fontWeight: '400',
+        color: '#FFFFFF', 
+    },
+    text: {
+        fontFamily: 'RobotoMono-Regular',
+        fontSize: 16,
+        color: '#FFFFFF', 
+    },
+    buttonContainer: {
+        marginTop: 20,
+        //padding: 20,
+    },
+    input: {
+        borderWidth: 1, // By default none
+        borderColor: '#777',
+        padding: 8,
+        margin: 10,
+        width: 200,
+    },
+    boxWrap: {
+        borderRadius: 15,
+        backgroundColor: 'rgba(4, 165, 255, 0.45)',
     }
 });
