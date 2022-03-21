e00c774d6e590e14c1e891b45836b36ef220e799
Robobot399 <phuc_l_nguyen@hotmail.com>
2022-03-21 13:08:36 +0000

    Finally Removed navigation header and title

--- Weather-App/navigation/DrawerNavigator.js
+++ Weather-App/navigation/DrawerNavigator.js
@@ -1,14 +1,5 @@
 import 'react-native-gesture-handler'; // MUST BE TOP
 import * as React from 'react';
-import { MaterialIcons} from '@expo/vector-icons';
-
-/*Pages*/
-import homePage from '../screens/homePage';
-import loginPage from '../screens/loginPage';
-import recommendationPage from '../screens/recommendationPage';
-import settingsPage from '../screens/settingsPage';
-import signUp from '../screens/signUp';
-import wardrobePage from '../screens/wardrobePage';
 
 /* Stacks, for drawer navigator to choose */
 import HomeStack from '../routes/homeStack';
@@ -18,22 +9,20 @@ import WardrobeStack from '../routes/wardrobeStack';
 import LoginStack from '../routes/loginStack';
 
 /*Navigation Settings*/
-import { createStackNavigator } from '@react-navigation/stack';
-  //import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-// For navigation bar
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-
-import { ScreenContainer } from 'react-native-screens';
-import { Button } from 'react-native-web';
-import { TabRouter } from 'react-navigation';
+  // import { ScreenContainer } from 'react-native-screens';
+  // import { Button } from 'react-native-web';
+  // import { TabRouter } from 'react-navigation';
 
 /* NAV BAR */
 const {Navigator, Screen} = createDrawerNavigator();
 
 export const RootDrawerNavigator = () => (
-    <Navigator initialRouteName="HomeScreen">
+    <Navigator 
+      initialRouteName="HomeScreen"
+      screenOptions={{headerTransparent: true, headerTitle: () => null}}
+    >
       <Screen
         name="HomeScreen"
         component={HomeStack}
--- Weather-App/routes/homeStack.js
+++ Weather-App/routes/homeStack.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { MaterialIcons} from '@expo/vector-icons';
 
+// Pages for home stack
 import HomePage from '../screens/homePage';
+
 
 const { Navigator, Screen } = createStackNavigator();
 
-export const HomeStack = () => (
-  <Navigator
+export const HomeStack = ({navigation}) => {
+
+  const openMenu = () => {
+    navigation.openDrawer();
+  }
+
+  return(
+    <Navigator
     headerMode='screen'
     screenOptions={{headerShown: false}}
   >
     <Screen
       name='Home'
       component={HomePage}
-      //options={{ title: 'Home Page' }}
+      options={{ 
+        //title: 'Home Page'
+        headerLeft: () => (
+          <MaterialIcons name="menu" size={28} color='black' onPress={openMenu}/>
+        ),
+        headerLeftContainerStyle: {paddingHorizontal: 20,}
+      }}
     />
   </Navigator>
-);
+  )
+  
+};
 
 export default HomeStack;
