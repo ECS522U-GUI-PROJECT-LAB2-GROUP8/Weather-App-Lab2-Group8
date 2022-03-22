3c49554b82e30a4df042f212bbfe5712b726814c
jing2 <jing2001@live.co.uk>
2022-03-22 01:32:33 +0000

    added mex temp and display days in an array

--- Weather-App/screens/homePage.js
+++ Weather-App/screens/homePage.js
@@ -28,6 +28,7 @@ const HomePage = () => {
         if(latitude && longitude) {
           fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`).then(response => response.json()).then(data => {
                 console.log(data)                             //Comment out once done
+                console.log("===================================================================================================================================")
                 //Temperature right now 
                 var tempValue = data['main']['temp']
                 setTemp(Math.round(tempValue))  
@@ -36,31 +37,70 @@ const HomePage = () => {
                 setWeatherType(data['weather']['0']['main'])
                 const weatherID = data['weather']['0']['id']
                 if (weatherID === 800) {                                //Clear: 800
-                    setIcon('https://i.imgur.com/vS6lWXt.png'); console.log("Cloudy")
+                    setIcon('https://i.imgur.com/vS6lWXt.png'); //console.log("Cloudy")
                 } else if ( weatherID >= 200 && weatherID < 300 ) {     //Thunderstorm: 2xx
-                    setIcon('https://i.imgur.com/5TLF554.png'); console.log("Thunderstorm")
+                    setIcon('https://i.imgur.com/5TLF554.png'); //console.log("Thunderstorm")
                 } else if ( weatherID >= 300 && weatherID < 600 ) {     //Drizzle/Rain: 3xx, 5xx
-                    setIcon('https://i.imgur.com/8XxXEbo.png'); console.log("Drizzle/Rain")
+                    setIcon('https://i.imgur.com/8XxXEbo.png'); //console.log("Drizzle/Rain")
                 } else if ( weatherID >= 600 && weatherID < 700 ) {     //Snow: 6xx
-                    setIcon('https://i.imgur.com/gs1CjgM.png'); console.log("Drizzle/Rain")
+                    setIcon('https://i.imgur.com/gs1CjgM.png'); //console.log("Drizzle/Rain")
                 } else if ( weatherID >= 700 && weatherID < 800 ) {     //Atmosphere: 7xx
-                    setIcon('https://i.imgur.com/NTr6AsO.png'); console.log("Drizzle/Rain")
+                    setIcon('https://i.imgur.com/NTr6AsO.png'); //console.log("Cloudy")
                 } else if ( weatherID > 800) {                          //Cloudy: 8xx
-                    setIcon('https://i.imgur.com/JdvzIwe.png'); console.log("Drizzle/Rain")
+                    setIcon('https://i.imgur.com/JdvzIwe.png'); //console.log("Drizzle/Rain")
                 }
          })
         }
       }
     
+    /**Icon useStates */
     const [day0, setDay0] = useState('')
+
+    /*Temp Usestates */ /*[MAX TEMP]*/
+    const [temp0, setTemp0] = useState('')
+    const [temp1, setTemp1] = useState('')
+    const [temp2, setTemp2] = useState('')
+    const [temp3, setTemp3] = useState('')
+    const [temp4, setTemp4] = useState('')
+    const [temp5, setTemp5] = useState('')
+    const [temp6, setTemp6] = useState('')
 
     //Fetching forecase data (7-Day)
     const fetchForecastData = (latitude, longitude) => {
         fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`).then(response => response.json()).then(data => {
+            //Get next 7 days UNIX 
+            var day00 = data['daily']['0']['dt']
+
+
+            //Get next 7 days MaxTemp
+            var temp00 = data['daily']['0']['temp']['max']
+            var temp11 = data['daily']['1']['temp']['max']
+            var temp22 = data['daily']['2']['temp']['max']
+            var temp33 = data['daily']['3']['temp']['max']
+            var temp44 = data['daily']['4']['temp']['max']
+            var temp55 = data['daily']['5']['temp']['max']
+            var temp66 = data['daily']['6']['temp']['max']
+ 
+            
+            setTemp0(Math.round(temp00))
+            setTemp1(Math.round(temp11))
+            setTemp2(Math.round(temp22))
+            setTemp3(Math.round(temp33))
+            setTemp4(Math.round(temp44))
+            setTemp5(Math.round(temp55))
+            setTemp6(Math.round(temp66))
+
+
             console.log(data)
-            var dayZero = new Date(data['daily']['0']['dt'] * 1000).toLocaleString('en-GB', {weekday: 'short'})     //Mon
-            console.log(dayZero)
-            setDay0(dayZero)
+
+            const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+            console.log(days[(new Date(day00 * 1000)).getUTCDay()])
+            console.log((new Date(day00 * 1000)))
+            // var dayZero = new Date()    //Mon
+            // console.log(dayZero)
+            // setDay0(dayZero)
+
         })
     }
   
@@ -94,32 +134,39 @@ const HomePage = () => {
                     </View>
                     <View style={weekForeCastContainer.container}>
                         <View style={individualDay.container}>
+                            <Text style={tempDaily.container} >{temp0}°C</Text>
                             <Image source={ require('../icons/sun_icon.png') } style={{width:30, height:30}}></Image>
-                            <Text>{day0}</Text>
+                            <Text style={textDay.container} >Mon</Text>
                         </View>
                         <View style={individualDay.container}>
+                            <Text style={tempDaily.container} >{temp1}°C</Text>
                             <Image source={ require('../icons/partial_sun_and_cloud_icon.png') } style={{width:30, height:30}}></Image>
-                            <Text>Tue</Text>
+                            <Text style={textDay.container} >Tue</Text>
                         </View>
                         <View style={individualDay.container}>
+                            <Text style={tempDaily.container} >{temp2}°C</Text>
                             <Image source={ require('../icons/cloud_icon.png') } style={{width:30, height:30}}></Image>
-                            <Text>Wed</Text>
+                            <Text style={textDay.container} >Wed</Text>
                         </View>
                         <View style={individualDay.container}>
+                            <Text style={tempDaily.container} >{temp3}°C</Text>
                             <Image source={ require('../icons/cloud_with_rain_icon.png') } style={{width:30, height:30}}></Image>
-                            <Text>Thu</Text>
+                            <Text style={textDay.container} >Thu</Text>
                         </View>
                         <View style={individualDay.container}>
+                            <Text style={tempDaily.container} >{temp4}°C</Text>
                             <Image source={ require('../icons/thunder_icon.png') } style={{width:30, height:30}}></Image>
-                            <Text>Fri</Text>
+                            <Text style={textDay.container} >Fri</Text>
                         </View>
                         <View style={individualDay.container}>
+                            <Text style={tempDaily.container} >{temp5}°C</Text>
                             <Image source={ require('../icons/sunny_with_fog_icon.png') } style={{width:30, height:30}}></Image>
-                            <Text>Sat</Text>
+                            <Text style={textDay.container} >Sat</Text>
                         </View>
                         <View style={individualDay.container}>
+                            <Text style={tempDaily.container} >{temp6}°C</Text>
                             <Image source={ require('../icons/snow_icon.png') }style={{width:30, height:30}}></Image>
-                            <Text>Sun</Text>
+                            <Text style={textDay.container} >Sun</Text>
                         </View>
                     </View>
             </ScrollView>
@@ -158,12 +205,12 @@ const weekForeCastContainer = StyleSheet.create({
         textAlign: 'center', 
         backgroundColor: ' rgba(0, 0, 0, 0.18)',
         width: '84%', 
-        minHeight: 300,
-        marginTop: 300 ,
+        minHeight: 120,
+        marginTop: 100 ,
         marginRight: '8%',
         marginLeft: '8%',
         borderRadius: 10,
-        padding: 10,
+        padding: 5,
         flexDirection: 'row',
     }
 })
@@ -172,6 +219,20 @@ const individualDay = StyleSheet.create({
     container: {
         marginLeft: '3%',
         marginRight: '3%',
+        marginTop: 20 ,
+    }
+})
+
+const textDay = StyleSheet.create({
+    container: {
+        color: 'white',
+        marginTop: 10
+    }
+})
+const tempDaily = StyleSheet.create({
+    container: {
+        color: 'white',
+        marginBottom: 10
     }
 })
 
